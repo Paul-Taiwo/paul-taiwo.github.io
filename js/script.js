0a53282b0f1dedc086889369a94d6fab1affb10b
Paul Taiwo <pauladedokun2000@gmail.com>
2019-03-24 17:57:23 +0100

    Update: Remove sass cache - add work section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-let body = document.querySelector('body');
-var colors = ['#6a1b9a', '#512da8', '#1565c0', '#ad1457', '#2e7d32'];
+let doc = document.documentElement,
+  body = document.querySelector('body'),
+  colors = ['#6a1b9a', '#512da8', '#1565c0', '#ad1457', '#2e7d32'];
 // var cc = +(localStorage.previousBGColor || -1) + 1
 // cc = cc >= colors.length ? 0 : cc;
 // body.style.backgroundColor = colors[cc];
 // localStorage.previousBGColor = cc;
-body.style.backgroundColor = colors[Math.floor(Math.random()*colors.length)]
+doc.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
