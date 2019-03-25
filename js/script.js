acccbbd5cd7dc17f96e35278d2e8ce7769f64af4
Paul Taiwo <pauladedokun2000@gmail.com>
2019-03-25 10:56:35 +0100

    Reformat code

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
-let doc = document.documentElement,
-  body = document.querySelector('body'),
-  colors = ['#6a1b9a', '#512da8', '#1565c0', '#ad1457', '#2e7d32'];
 // var cc = +(localStorage.previousBGColor || -1) + 1
 // cc = cc >= colors.length ? 0 : cc;
 // body.style.backgroundColor = colors[cc];
 // localStorage.previousBGColor = cc;
-doc.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-if (doc.style.backgroundColor == 'rgb(106, 27, 154)') {
+
+let doc = document.documentElement,
+    body = document.querySelector('body'),
+    docBgColor,
+    colors = ['#6a1b9a', '#512da8', '#1565c0', '#ad1457', '#2e7d32'];
+
+docBgColor = doc.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+if (docBgColor == '#6a1b9a') {
   body.style.backgroundColor = '#804ca0';
-} else if (doc.style.backgroundColor == 'rgb(81, 45, 168)') {
+} else if (docBgColor == '#512da8') {
   body.style.backgroundColor = '#5c6bc0';
-} else if (doc.style.backgroundColor == 'rgb(21, 101, 192)') {
+} else if (docBgColor == '#1565c0') {
   body.style.backgroundColor = '#2085d6';
-} else if (doc.style.backgroundColor == 'rgb(173, 20, 87)') {
+} else if (docBgColor == '#ad1457') {
   body.style.backgroundColor = '#dc2b78';
-} else if (doc.style.backgroundColor == 'rgb(46, 125, 50)') {
+} else if (docBgColor == '#2e7d32') {
   body.style.backgroundColor = '#3da943';
 }
