b797e1c5d115d36b95ab2db8cc3391acb4afddba
Paul Taiwo <pauladedokun2000@gmail.com>
2019-03-25 10:42:42 +0100

    Add conditions for background color

--- js/script.js
+++ js/script.js
@@ -5,4 +5,15 @@ let doc = document.documentElement,
 // cc = cc >= colors.length ? 0 : cc;
 // body.style.backgroundColor = colors[cc];
 // localStorage.previousBGColor = cc;
-doc.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
+doc.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+if (doc.style.backgroundColor == 'rgb(106, 27, 154)') {
+  body.style.backgroundColor = '#804ca0';
+} else if (doc.style.backgroundColor == 'rgb(81, 45, 168)') {
+  body.style.backgroundColor = '#5c6bc0';
+} else if (doc.style.backgroundColor == 'rgb(21, 101, 192)') {
+  body.style.backgroundColor = '#2085d6';
+} else if (doc.style.backgroundColor == 'rgb(173, 20, 87)') {
+  body.style.backgroundColor = '#dc2b78';
+} else if (doc.style.backgroundColor == 'rgb(46, 125, 50)') {
+  body.style.backgroundColor = '#3da943';
+}
